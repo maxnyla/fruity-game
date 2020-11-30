ecd7499ecbc3becef279aeca9a99c79a69d29ea7
maxnyla <sariflor@yahoo.co.uk>
2020-11-30 21:13:06 +0000

    added to script.js file

--- scripts/scripts_copy_copy.js
+++ scripts/scripts_copy_copy.js
@@ -1,44 +1,83 @@
 const cards = document.querySelectorAll('.fruit-card');
 
-let hasFlippedCard = false;
+let cardFlipped = false;
+let blockClick = false;
 let firstCard, secondCard;
 
 
-//function flipCard(){
-  // this.classList.toggle('flip');
-//}
-
 
 function flipCard(){
+  if (blockClick) return;
+  if (this === firstCard) return;
+
   this.classList.add('flip');
   
-  if (!hasFlippedCard) {
+  if (!cardFlipped) {
     // first clicked card
-    hasFlippedCard = true;
+    cardFlipped = true;
     firstCard = this;
   } else {
     //second clicked card
-    hasFlippedCard = false;
+    cardFlipped = false;
     secondCard = this;
+    
+    doCardsMatch();
 
-    //do the two cards match?
-    if (firstCard.dataset.frame ===
-    secondCard.dataset.frame) {
+}
+}
 
-      //you matched them
-      firstCard.removeEventListener('click, flipCard');
-      secondCard.removeEventListener('click, flipCard');
-    } else {
-      //cards don't match
-      setTimeout(() => {
+function doCardsMatch(){
+    //do the two cards match? 
+   if (firstCard.dataset.frame ===
+       secondCard.dataset.frame) {
+      //they match:
+       removeCards();
+     //they don't match:
+     } else {
+       unflipCards();
+  }            
+}
+
+
+
+
+function removeCards(){
+     firstCard.removeEventListener('click, flipCard');
+     secondCard.removeEventListener('click, flipCard');
+
+     resetGame();
+}
+
+
+function unflipCards(){
+        blockClick = true;
+
+        setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+
+        blockClick = false;
+        resetGame();
       }, 1000);
-    }
-
 }
+         
+
+function resetGame(){
+  [cardFlipped, blockClick] = [false, false];
+  [firstCard, secondCard] = [null, null];
 }
 
 
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;  
+  });
+})();
 
+                    
 cards.forEach(card => card.addEventListener('click', flipCard));
+
+
+
+                 
